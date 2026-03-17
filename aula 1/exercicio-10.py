c97f25c6d7deb0efa4ea3207bc6e9abf527e2da1
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 22:32:01 -0300

    exercicio 11

--- aula 1/exercicio-10.py
+++ aula 1/exercicio-10.py
@@ -1,10 +1,12 @@
 salario = float(input("quanto o senhor(ou senhora) ganha? "))
 
 if salario <= 1621:
-    print(f"com os descontos do inss, voce ganhara{salario}")
+    print(f"com os descontos do inss, voce ganhara {salario}")
 elif salario >= 1621.01 and salario <=2902.84:
-    print(f"com os decontos do inss, voce ganha{salario-24.32}")
+    print(f"com os decontos do inss, voce ganha {salario-24.32}")
 elif salario >= 2902.85 and salario <=4354.27:
-    print(f"com os decontos do inss, voce ganha{salario-111.40}")
+    print(f"com os decontos do inss, voce ganha {salario-111.40}")
 elif salario >= 4354.28 and salario <=8475.55:
-    print(f"com os decontos do inss, voce ganha{salario-198.49}")
+    print(f"com os decontos do inss, voce ganha {salario-198.49}")
+else: 
+    print("muita grana pro meu codigo saber quanto que é")
---
+++ exercicio-11.py
@@ -0,0 +1,9 @@
+score = int(input("qual é seu score do serasa "))
+renda = int(input("qual é sua renda mensal "))
+
+if renda >= 3000 and score >=700:
+    print("ebaaaaaaaaaaaaaaaaaaaaaaaaaaaaa parbens aprovado ebaaaaaaaaaaaaaaaaaaaaaaaaa ")
+elif renda >= 2000 and score >= 600:
+    print("estamos verificando manualmente! aguarde na linha ")
+else:
+    print("acesso negado. ")
